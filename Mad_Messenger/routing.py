e8cb273c1108fe64c4c61e134e84c962065fdfbe
coderslav <coderslav>
2021-09-01 23:56:33 +0200

    Mad_Messenger 2.9 (Initialization - Add index page+CSS + little cosmetics changes)

--- Mad_Messenger/routing.py
+++ Mad_Messenger/routing.py
@@ -1,8 +1,14 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns
+# TODO раскомментировать закомментированное?
+# from django.core.asgi import get_asgi_application
+# import os
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myChat.settings')
 
 application = ProtocolTypeRouter({
+    # "http": get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
